8d8a15709918f440fa6e6b9357e9ae7b1b5f4b74
Juan Narvaez <juan.narvaez@correounivall.edu.co>
2017-06-08 15:00:49 -0500

    Slider con error, preguntar margi

---
+++ src/main/slider.js
@@ -0,0 +1,49 @@
+var pointer = 0;
+
+$(document).on('ready', function(){
+	
+	$('.bt_left').on('click', function(e){
+		e.preventDefault();
+		moveSlider('left');
+
+	});
+
+	console.log("paso por el bt_left");
+
+
+	$('.bt_right').on('click', function(e){
+		e.preventDefault();
+		moveSlider('right');
+	});
+
+	console.log("paso por el bt_right");
+
+	console.log("Hola k ase");
+
+	var mensaje = $('.form_container').position();
+	console.log(mensaje);
+
+});
+
+
+
+
+function moveSlider(direction){
+	var limit = $('.form_container .slide').length;
+
+	pointer = (direction == 'right') ? pointer + 1 : pointer-1;
+	pointer = (direction >= limit) ? 0 : pointer ;
+	pointer = (direction < 0 ) ? limit - 1 : pointer ;
+
+	var mensaje = -(pointer * $('.form_container').width())+"px";
+	console.log(mensaje);
+
+	$('.form_container .slide_container').animate({
+		'margin-left': -(pointer * $('.form_container').width())+"px"
+
+	});
+
+
+}
+
+
